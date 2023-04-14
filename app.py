039083d375f8b31e0cf9546a0f476dd48df875e1
sukruburakcetin <cetin.sukruburak@gmail.com>
2023-04-14 18:19:16 +0300

    init core elements

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request, jsonify
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/save-points", methods=["POST"])
+def save_points():
+    # Get the points data from the AJAX request
+    points = request.json.get('points')
+    print(points)  # do something with the points data here
+    return {"success": True}
+
+if __name__ == "__main__":
+    app.run(debug=True)
