35c647228c7b9cfe4268c5f111f4fb4106fc20e1
sukruburakcetin <cetin.sukruburak@gmail.com>
2023-04-15 11:24:17 +0300

    polygon are created and stored  based on user's points

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, jsonify
+from shapely import Point
 
 app = Flask(__name__)
 
@@ -13,7 +14,21 @@ def save_points():
     # Get the points data from the AJAX request
     points = request.json.get('points')
     print(points)  # do something with the points data here
-    return {"success": True}
+    # center point provided by the user
+    point = Point(points['geometry']['coordinates'][0], points['geometry']['coordinates'][1])
+    # Create a buffer around the Point object with a radius of 500 meters
+    polygon = point.buffer(0.0003) #0.005 is roughly equivalent to 500 meters
+    # Convert the Shapely Polygon object to a GeoJSON Feature object
+    feature = {
+        'type': 'Feature',
+        'geometry': {
+            'type': 'Polygon',
+            'coordinates': [list(polygon.exterior.coords)]
+        }
+    }
+    print(jsonify(feature))
+    # Return the GeoJSON Feature object as a JSON response
+    return jsonify(feature)
 
 
 if __name__ == "__main__":
