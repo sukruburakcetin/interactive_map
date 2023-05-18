a2521c2ee5c13d162cf8461eef4816eeeb793712
sukruburakcetin <cetin.sukruburak@gmail.com>
2023-05-18 09:04:38 +0300

    some pep8 fixes

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
 import json
 
+import cx_Oracle
 import pandas as pd
-import requests
 from flask import Flask, render_template, request, jsonify
 from shapely import Point
 from shapely.geometry import shape
-import cx_Oracle
 
 app = Flask(__name__)
 
@@ -112,25 +111,34 @@ def save_points():
             })
             index_no += 1
 
-        # Calculate the weighted average of the suitability values based on the proportion of the polygon within each shape
+        # Calculate the weighted average of the suitability values based
+        # on the proportion of the polygon within each shape
         total_proportion = sum([value['proportion_within_shape'] for value in suitability_values])
         weighted_suitability_values = []
         for value in suitability_values:
             weighted_suitability_values.append({
                 'weighted_suitability': value['gridcode'] * (value['proportion_within_shape'] / total_proportion)
-                # burada kesisen altlık polygonlarının gridcodelarını(yani uygunluk kodlarını), yukarda hesapladığım proporsiyonla isleme sokuyorum
+                # burada kesisen altlık polygonlarının gridcodelarını(yani uygunluk kodlarını),
+                # yukarda hesapladığım proporsiyonla isleme sokuyorum
             })
 
-        # her bir polygondan 1.3 ve 1.6 gibi degerler geliyor, hangisinde daha çok kesişmisse o daha büyük oluyor( değerlerin 3'ün altında gelmesi yukardaki işlemde normalize edilmesinden yani oradaki 2 polygonla kesisiyor ya onların 2 ve 3 geliyor bunların agırlıkları diyelim)
-        # bunları topluyorum GRIDCODE: 2.5532738460809687 böyle geliyor bu da ne  2 ne 3 ama 2li olanından ne kadar 2liği o alanla kesisiyor ne kadar 3lüğü o alanla kesisiyor bu sayı artıyor ya da  azalıyor ama 2 ve 3 le kesistiği için 2 ile 3 arasında oluyor
-        # eger polygonun alanı 2 lik kısımda daha çok olsaydı 2 ye daha yakın olacaktı ama burda 3 lük kırmızı alanda o yuvarlak polygon daha cok oldugu için 2.5 gibi geldi
+        # her bir polygondan 1.3 ve 1.6 gibi degerler geliyor, hangisinde daha çok kesişmisse o daha büyük
+        # oluyor( değerlerin 3'ün altında gelmesi yukardaki işlemde normalize edilmesinden yani oradaki
+        # 2 polygonla kesisiyor ya onların 2 ve 3 geliyor bunların agırlıkları diyelim)
+        # bunları topluyorum GRIDCODE: 2.5532738460809687 böyle geliyor bu da ne  2 ne 3 ama 2li
+        # olanından ne kadar 2liği o alanla kesisiyor ne kadar 3lüğü o alanla kesisiyor bu sayı artıyor
+        # ya da  azalıyor ama 2 ve 3 le kesistiği için 2 ile 3 arasında oluyor
+        # eger polygonun alanı 2 lik kısımda daha çok olsaydı 2 ye daha yakın olacaktı ama burda
+        # 3 lük kırmızı alanda o yuvarlak polygon daha cok oldugu için 2.5 gibi geldi
         # Calculate the final suitability value as the sum of the weighted suitability values
-
+        suitability = ""
         # sonra burada toplatılmıs value'ye göre tekrardan uygunluk description ataması yapıyorum
         final_suitability_value = sum([value['weighted_suitability'] for value in weighted_suitability_values])
-        if 3 < final_suitability_value < 4:  # misal bizim GRIDCODE: 2.5532738460809687 degeri bu aralıkta o yüzden uygun - az uygun
+        if 3 < final_suitability_value < 4:  # misal bizim GRIDCODE: 2.5532738460809687 degeri
+            # bu aralıkta o yüzden uygun - az uygun
             suitability = "Uygun"
-        elif 2 < final_suitability_value < 3:  # misal bizim GRIDCODE: 2.5532738460809687 degeri bu aralıkta o yüzden uygun - az uygun
+        elif 2 < final_suitability_value < 3:  # misal bizim GRIDCODE: 2.5532738460809687 degeri bu
+            # aralıkta o yüzden uygun - az uygun
             suitability = "Uygun - Az Uygun"
 
         elif 1 < final_suitability_value < 2:
